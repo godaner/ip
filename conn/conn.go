a536d4b2017cfe31f6c4329f865f41aefb053f69
zhangke <zhangke>
2020-01-21 15:04:43 +0800

    lock close conn

--- conn/conn.go
+++ conn/conn.go
@@ -1,10 +1,15 @@
 package conn
 
-import "net"
+import (
+	"net"
+	"sync"
+)
+
 // IPConn
 type IPConn struct {
 	net.Conn
 	isClose chan int
+	sync.Mutex
 }
 
 func NewIPConn(conn net.Conn) *IPConn {
@@ -16,34 +21,20 @@ func NewIPConn(conn net.Conn) *IPConn {
 func (i *IPConn) Read(b []byte) (n int, err error) {
 	n, err = i.Conn.Read(b)
 	if err != nil && i.isClose != nil {
-		select {
-		case <-i.isClose:
-		default:
-			close(i.isClose)
-		}
+		i.close()
 	}
 	return n, err
 }
 func (i *IPConn) Write(b []byte) (n int, err error) {
 	n, err = i.Conn.Write(b)
-	if err != nil && i.isClose != nil {
-		select {
-		case <-i.isClose:
-		default:
-			close(i.isClose)
-		}
+	if err != nil {
+		i.close()
 	}
 	return n, err
 }
 func (i *IPConn) Close() error {
 	err := i.Conn.Close()
-	if i.isClose != nil {
-		select {
-		case <-i.isClose:
-		default:
-			close(i.isClose)
-		}
-	}
+	i.close()
 	return err
 }
 
@@ -52,4 +43,17 @@ func (i *IPConn) IsClose() (c chan int) {
 		i.isClose = make(chan int)
 	}
 	return i.isClose
+}
+
+func (i *IPConn) close() {
+	if i.isClose == nil {
+		return
+	}
+	i.Lock()
+	select {
+	case <-i.isClose:
+	default:
+		close(i.isClose)
+	}
+	i.Unlock()
 }
